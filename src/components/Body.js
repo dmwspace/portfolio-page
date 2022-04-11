4c045c47382d13fc60f0dbd24404e6f842a2082d
dean <dean@deans-iMac.home>
2022-04-11 12:14:12 -0700

    Created the navbar elements.

---
+++ src/components/Body.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Body() {
+    return (
+        <div id="body">
+            <h1>Hello, my name is Dean Wright. I am a Web Developer based in Shoreline, WA.</h1>
+        </div>
+    )
+}
+export default Body
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <div id="navbar">
+            <li id="name">Dean Wright</li>
+            <ul id="navbar-items">
+                <li>Home</li>
+                <li>Projects</li>
+                <li>Resume</li>
+            </ul>
+
+        </div>
+    )
+}
+export default Navbar
