eaa5082f394f609e6f89071cdda3b2a996c845e9
dean <dean@deans-iMac.home>
2022-06-17 23:59:28 -0700

    working on skills page

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Card, ThemeProvider, Container, Row, Col, ListGroup } from 'react-bootstrap';
 
 export default function Skills() {
     return (
-        <div className="skills-container">
-            <h1 className="skills-text">This is the Skills page</h1>
-        </div>
+        <ThemeProvider breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}>
+            <Container>
+                <Row sm={1} md={2}>
+                    <Col>
+                        <Card style={{width: '18rem'}}>
+                            <Card.Title>Technical Skills</Card.Title>
+                            <ListGroup variant="flush">
+                                <ListGroup.Item>Javascript</ListGroup.Item>
+                                <ListGroup.Item>React</ListGroup.Item>
+                                <ListGroup.Item>Node</ListGroup.Item>
+                                <ListGroup.Item>Express</ListGroup.Item>
+                                <ListGroup.Item>Git</ListGroup.Item>
+                                <ListGroup.Item>HTML</ListGroup.Item>
+                                <ListGroup.Item>CSS</ListGroup.Item>
+                                <ListGroup.Item>Responsive Web Design</ListGroup.Item>
+                            </ListGroup>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card style={{width: '18rem'}}>
+                            <Card.Title>Professional Skills</Card.Title>
+                            <ListGroup variant="flush"
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </ThemeProvider>
+            
     )
 }
